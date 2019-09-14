215bfc8b19c92785d5f5dcc1290afdf9d707430a
Adam Carson <adam.carson@lvlsvn.com>
2019-09-14 15:31:14 -0400

    Movie and Movielist controller, updated routes, response helper

---
+++ app/controllers/movielists_controller.rb
@@ -0,0 +1,37 @@
+class MovielistsController < ApplicationController
+	before_action :set_movielist, only: [:show, :update, :destroy]
+
+	def index
+		@movielists = Movielist.all
+		json_response(@movielists)
+	end
+
+	def create
+		@movielist = Movielist.create!(movielist_params)
+		json_response(@movielist, :created)
+	end
+
+	def show
+		json_response(@movielist)
+	end
+
+	def update
+		@movielist.update(movielist_params)
+		head :no_content
+	end
+
+	def destroy
+		@movielist.destroy
+		head :no_content
+	end
+
+	private
+
+	def movielist_params
+		params.permit(:name)
+	end
+
+	def set_movielist
+		@movielist = Movielist.find(params[:id])
+	end
+end
